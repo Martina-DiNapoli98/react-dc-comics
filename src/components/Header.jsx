e8479dd381cae955bf8357c56f32262f9e0d5c92
Martina-dinapoli98 <martinadinapoli1998@gmail.com>
2025-03-20 16:46:32 +0100

    add main with sections up and down

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+export default function Header() {
+    return (
+        <header>
+            <div className="container">
+                <img src="./dc-logo.png" alt="" />
+                <ul>
+                    <li>Characters</li>
+                    <li>comics</li>
+                    <li>movies</li>
+                    <li>tv</li>
+                    <li>games</li>
+                    <li>collectibles</li>
+                    <li>videos</li>
+                    <li>fans</li>
+                    <li>news</li>
+                    <li>shop</li>
+                </ul>
+            </div>
+
+        </header>
+    )
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,56 @@
+export default function Main() {
+    return (
+        <main>
+            <section className="up">
+                <h3 className="content">
+                    Content goes here
+                </h3>
+            </section>
+            <section className="down">
+                <ul>
+                    <li>
+                        <a href="">
+                            <img className="icon" src="./buy-comics-digital-comics.png" alt="" width={30} />
+                            <span>
+                                DIGITAL COMICS
+                            </span>
+                        </a>
+
+                    </li>
+                    <li>
+                        <a href="">
+                            <img className="icon" src="./buy-comics-merchandise.png" alt="" width={30} />
+                            <span>
+                                DC MERCHANDISE
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img className="icon" src="./buy-comics-subscriptions.png" alt="" width={30} />
+                            <span>
+                                SUBSCRIPTIONS
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img className="icon" src="./buy-comics-shop-locator.png" alt="" width={20} />
+                            <span>
+                                COMIC SHOP LOCATOR
+                            </span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="">
+                            <img className="icon" src="./buy-dc-power-visa.svg" alt="" width={30} />
+                            <span>
+                                DC POWER VISA
+                            </span>
+                        </a>
+                    </li>
+                </ul>
+            </section>
+        </main>
+    )
+};
